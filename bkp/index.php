742f59c6d069a7a7fa60c8d358f65273a92fc3b0
Alexey Vasilyev <bestspajic@gmail.com>
2013-10-20 23:53:58 +0400

    backup old index.php

---
+++ bkp/index.php
@@ -0,0 +1,66 @@
+ 
+<div class="page_align"> 
+  <table style="border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0" align="center"> 
+    <tbody> 
+      <tr><td> 
+          <table style="border-collapse: collapse;" border="0" cellspacing="0" cellpadding="0"> 
+            <tbody> 
+              <tr><td height="10"><font size="2" face="Arial"></font></td></tr>
+             
+              <tr><td align="left"> 
+                  <table style="border-collapse: collapse;" border="0" cellspacing="0" cellpadding="2" width="480"> 
+                    <tbody> 
+                      <tr><td><a href="472.php" style="color: rgb(0, 0, 0); font-weight: bold;"><font size="2" face="Arial"><img src="/images_dep/78/lopatin_1_sm.gif" border="0"/></font></a></td><td align="center"><a style="color: rgb(0, 0, 0); font-weight: bold;" href="http://portal6.gubkin.ru/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/472.php"><font size="2" face="Arial">Лопатин Алексей Сергеевич, 
+                              <br />
+                             заведующий кафедрой,&nbsp;профессор, &nbsp;д.т.н.</font></a></td></tr>
+                     
+                      <tr><td><a href="/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/473.php" style="color: rgb(0, 0, 0); font-weight: bold;"><font size="2" face="Arial"><img src="/images_dep/78/shotidi_1_sm.gif" border="0"/></font></a></td><td align="center"><a style="color: rgb(0, 0, 0); font-weight: bold;" href="http://portal6.gubkin.ru/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/473.php"><font size="2" face="Arial">Шотиди Константин Харлампиевич, 
+                              <br />
+                             заместитель заведующего кафедрой, профессор, к.т.н.</font></a></td></tr>
+                     
+                      <tr><td><a href="474.php" style="color: rgb(0, 0, 0); font-weight: bold;"><font size="2" face="Arial"><img src="/images_dep/78/kalinin_1_sm.gif" border="0"/></font></a></td><td align="center"><a style="color: rgb(0, 0, 0); font-weight: bold;" href="http://portal6.gubkin.ru/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/474.php"><font size="2" face="Arial">Калинин Александр Федорович, 
+                              <br />
+                             заместитель заведующего кафедрой по научной работе, профессор, д.т.н.</font></a></td></tr>
+                     
+                      <tr><td><a href="/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/478.php"><img src="/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/images/N.H.3.jpg" width="50" height="60"/></a>&nbsp;</td><td> 
+                          <p align="center">&nbsp;<a style="color: rgb(0, 0, 0); font-weight: bold;" href="http://portal6.gubkin.ru/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/478.php"><font size="2" face="Arial">Халлыев Назар Халлыевич, 
+                                <br />
+                               профессор, д.т.н.</font></a></p>
+                         </td><td align="center"><a href="http://www.gubkin.ru/bitrix/admin/474.php" style="color: rgb(0, 0, 0); font-weight: bold;"><font size="2" face="Arial"></font></a></td></tr>
+                     
+                      <tr><td><a href="/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/475.php" style="color: rgb(0, 0, 0); font-weight: bold;"><font size="2" face="Arial"><img src="/images_dep/78/kuptsov_1_sm.gif" border="0"/></font></a></td><td align="center"><a style="color: rgb(0, 0, 0); font-weight: bold;" href="http://portal6.gubkin.ru/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/475.php"><font size="2" face="Arial">Купцов Сергей Михайлович, 
+                              <br />
+                             профессор, д.т.н.</font></a></td></tr>
+                     
+                      <tr><td><a href="/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/479.php" style="color: rgb(0, 0, 0); font-weight: bold;"><font size="2" face="Arial"><img src="/images_dep/78/mika_1_sm.gif" border="0"/></font></a></td><td align="center"><a style="color: rgb(0, 0, 0); font-weight: bold;" href="http://portal6.gubkin.ru/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/479.php"><font size="2" face="Arial">Микаэлян Эдуард Амаякович, 
+                              <br />
+                             доцент, к.т.н.</font></a></td></tr>
+                     
+                      <tr><td><a href="/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/481.php" style="color: rgb(0, 0, 0); font-weight: bold;"><font size="2" face="Arial"><img src="/images_dep/78/nazar_1_sm.gif" border="0"/></font></a></td><td align="center"><a style="color: rgb(0, 0, 0); font-weight: bold;" href="http://portal6.gubkin.ru/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/481.php"><font size="2" face="Arial">Назарьина Александра Михайловна, 
+                              <br />
+                             доцент, к.т.н.</font></a></td></tr>
+                     
+                      <tr><td><a href="/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/477.php" style="color: rgb(0, 0, 0); font-weight: bold;"><font size="2" face="Arial"><img src="/images_dep/78/shprot_1_sm.gif" border="0"/></font></a></td><td align="center"><a style="color: rgb(0, 0, 0); font-weight: bold;" href="http://portal6.gubkin.ru/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/477.php"><font size="2" face="Arial">Шпотаковский Михаил Михайлович, 
+                              <br />
+                             доцент, к.т.н.</font></a></td></tr>
+                     
+                      <tr><td>&nbsp;<a href="/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/480.php"><img src="/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/images/123 DSC_0052.PNG" border="0" width="50" height="50"/></a></td><td> 
+                          <p align="center">&nbsp;<a style="color: rgb(0, 0, 0); font-weight: bold;" href="http://portal6.gubkin.ru/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/480.php"><font size="2" face="Arial">Чурикова Мария Михайловна, 
+                                <br />
+                               доцент, к.т.н.</font></a></p>
+                         </td></tr>
+                     
+                      <tr><td><a href="/faculty/pipeline_network_design/chairs_and_departments/thermodynamics_and_thermal_engine/482.php"></a></td><td align="center"> 
+                          <p align="center"></p>
+                         </td></tr>
+                     </tbody>
+                   </table>
+                 </td></tr>
+             </tbody>
+           </table>
+         </td></tr>
+     </tbody>
+   </table>
+ 
+  <br />
+ </div>
